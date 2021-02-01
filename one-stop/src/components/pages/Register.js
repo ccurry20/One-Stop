90633ccca0f52dce9214f63848bca79448e409b0
Carmen <ccurry20@gmail.com>
2021-01-31 19:31:14 -0500

    Login Form css

--- one-stop/src/components/pages/Register.js
+++ one-stop/src/components/pages/Register.js
@@ -1,16 +1,16 @@
 import React from "react";
 import RegisterHeader from "../RegisterHeader";
-import LoginForm from "../LoginForm";
+import RegistrationForm from "../RegistrationForm";
 import "../../App.css";
 
 function Login() {
   return (
-    <div className="App">
+    <>
       <RegisterHeader />
-      <div className="container d-flex align-items-center flex-column">
-        <LoginForm />
+      <div>
+        <RegistrationForm />
       </div>
-    </div>
+    </>
   );
 }
 
--- one-stop/src/components/pages/Search.js
+++ one-stop/src/components/pages/Search.js
@@ -2,20 +2,10 @@ import React from "react";
 import BlogMain from "../BlogMain";
 import Footer from "../Footer";
 import "../../App.css";
-import { Button } from "../Button";
-import CreateArticle from "../CreateArticle";
 
 function Search() {
   return (
     <>
-      <Button
-        // className="btns"
-        // buttonStyle="btn--outline"
-        // buttonSize="btn--large"
-        link={CreateArticle}
-      >
-        CREATE LISTING
-      </Button>
       <BlogMain />
       <Footer />
     </>
